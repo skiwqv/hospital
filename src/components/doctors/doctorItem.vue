3aa3d11c3fa8e95af37314621e9476f57f346117
skiwqv <andreus0212@gmail.com>
2024-09-07 18:37:30 +0300

    added carousel component

---
+++ src/components/doctors/doctorItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="doctor-wrapper">
+    <img class="doctor-avatar" :src="doctor.avatar" alt="" />
+    <div class="info-wrapper">
+      <span class="doctor-name">{{ doctor.name }}</span>
+      <h5 class="doctor-speciality">{{ doctor.speciality }}</h5>
+      <div class="social-wrapper">
+        <linkedinIcon class="social-icon"></linkedinIcon>
+        <facebookIcon class="social-icon"></facebookIcon>
+        <instagramIcon class="social-icon"></instagramIcon>
+      </div>
+      <button class="doctor-button">View Profile</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import linkedinIcon from "../../assets/icons/linkdeIn.svg";
+import facebookIcon from "../../assets/icons/facebook.svg";
+import instagramIcon from "../../assets/icons/instagram.svg";
+const props = defineProps({
+  doctor: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
