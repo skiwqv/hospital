851b6aec5b3223adf8ed318738bdf4c14cd3abca
skiwqv <andreus0212@gmail.com>
2024-09-10 23:34:01 +0300

    added user registration and login

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,18 @@
 import axios from "axios";
+import { getTokenFromCookies } from "../helpers/Cookies";
+const token = getTokenFromCookies("access");
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVICE_URL,
 });
 
-export default apiClient;
+const authorizedApiClient = axios.create({
+  baseURL: import.meta.env.VITE_APP_SERVICE_URL,
+});
+
+if (token) {
+  authorizedApiClient.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${token}`;
+}
+
+export { apiClient, authorizedApiClient };
